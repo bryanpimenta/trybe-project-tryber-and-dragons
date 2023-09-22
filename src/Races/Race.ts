d1106e11bd45ee8dfa7182a2e5bcc7167ac56453
Bryan Pimenta <bryanpimenta30@gmail.com>
2023-09-22 13:37:41 -0300

    feat (created extendeds: dwarf, elf, halfling, orc)

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,19 +1,17 @@
 abstract class Race {
   private _name: string;
   private _dexterity: number;
-  private static _instances = 0;
 
   constructor(name: string, dexterity: number) {
     this._name = name;
     this._dexterity = dexterity;
-    Race._instances += 1;
   }
 
   get name(): string { return this._name; }
 
   get dexterity(): number { return this._dexterity; }
 
-  static createdRacesInstances() {
+  static createdRacesInstances(): number {
     throw new Error('Not implemented');
   }
 
