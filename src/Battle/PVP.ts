982df94b3a0983d65d0596e24bb4e0e2d711989e
Bryan Pimenta <bryanpimenta30@gmail.com>
2023-09-25 13:04:51 -0300

    feat (class PVP)

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,25 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+class PVP extends Battle {
+  private _anotherPlayer: Fighter;
+
+  constructor(player: Fighter, anotherPlayer: Fighter) {
+    super(player);
+    this._anotherPlayer = anotherPlayer;
+  }
+
+  override fight(): number {
+    while (this.player.lifePoints > 0 && this._anotherPlayer.lifePoints > 0) {
+      this.player.attack(this._anotherPlayer);
+      this._anotherPlayer.attack(this.player);
+    }
+    
+    if (this.player.lifePoints === -1) {
+      return -1;
+    } 
+    return 1;
+  }
+}
+
+export default PVP;
